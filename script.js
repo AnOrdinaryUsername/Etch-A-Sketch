c8318de7dd8948680327b309672bdb7ae4b16e49
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-05-29 16:32:33 -0700

    Include interactivity with reset button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-// https://stackoverflow.com/questions/59793332/vanilla-js-hover-over-div-change-background-color
 function createGrid(sizeOfGrid) {
     // Split the entire grid (100% of it) by the amount of boxes
     const splitEvenly = 100 / sizeOfGrid;
@@ -15,16 +13,44 @@ function createGrid(sizeOfGrid) {
     } 
 }
 
-function addTrail() {
+function addTrail(color) {
     document.querySelector(".etch-a-sketch").addEventListener('mouseover', function(e) {
-        // e.target was the clicked element
-       e.target.classList.add('color');
+        // e.target was the mouseovered div box element
+        e.target.style.background = `${color}`;
     }, false);
 }
 
+function resetGrid(color) {
+    document.querySelector(".reset-shake").addEventListener('click', function(e) {
+        let totalNodes = document.querySelector('.etch-a-sketch').childElementCount;
+
+        let etchToy = document.querySelector('.etch-wrapper');
+        // Add shake up and down animation to etch-wrapper
+        etchToy.classList.add('shake');
+        
+        for (let i = 0; i < totalNodes; i++) {
+            let box = document.getElementsByClassName('box')[i];
+
+            // Remove inline styles that have a colored background
+            // (resets to original lightgray property)
+            if (box.style.background == `${color}`) 
+                box.style.removeProperty('background');
+        }
+
+        setTimeout(function () {
+            etchToy.classList.remove('shake');
+        }, 10);
+    });
+}
+
+function randomizeColor() {
+    
+}
+
 function main() {
-    createGrid(16);
-    addTrail();
+    createGrid(64);
+    resetGrid('blue');
+    addTrail('blue');
 }
 
 main();
