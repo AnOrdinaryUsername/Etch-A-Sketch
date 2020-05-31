28d05442f1e000556eb4bc8e6ea7591e297d391e
AnOrdinaryUsername <kylemasa2@gmail.com>
2020-05-30 23:07:05 -0700

    Make user input work with the DOM

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create grid boxes in Etch-A-Sketch
 function createGrid(sizeOfGrid) {
     // Split the entire grid (100% of it) by the amount of boxes
     const splitEvenly = 100 / sizeOfGrid;
@@ -13,44 +14,109 @@ function createGrid(sizeOfGrid) {
     } 
 }
 
-function addTrail(color) {
-    document.querySelector(".etch-a-sketch").addEventListener('mouseover', function(e) {
-        // e.target was the mouseovered div box element
-        e.target.style.background = `${color}`;
-    }, false);
-}
+// Remove all color from grid boxes and resets them back to lightgray
+function resetGrid() {
+    document.querySelector('.reset-shake').addEventListener('click', function(e) {
+    let totalNodes = document.querySelector('.etch-a-sketch').childElementCount;
 
-function resetGrid(color) {
-    document.querySelector(".reset-shake").addEventListener('click', function(e) {
-        let totalNodes = document.querySelector('.etch-a-sketch').childElementCount;
-
-        let etchToy = document.querySelector('.etch-wrapper');
-        // Add shake up and down animation to etch-wrapper
-        etchToy.classList.add('shake');
+    let etchToy = document.querySelector('.etch-wrapper');
+    // Add shake up and down animation to etch-wrapper
+    etchToy.classList.add('shake');
         
-        for (let i = 0; i < totalNodes; i++) {
-            let box = document.getElementsByClassName('box')[i];
+    for (let i = 0; i < totalNodes; i++) {
+        let box = document.getElementsByClassName('box')[i];
 
-            // Remove inline styles that have a colored background
-            // (resets to original lightgray property)
-            if (box.style.background == `${color}`) 
-                box.style.removeProperty('background');
-        }
-
-        setTimeout(function () {
-            etchToy.classList.remove('shake');
-        }, 10);
+        // Remove inline styles that have a colored background
+        // (resets to original lightgray property)
+        if (box.style.background != 'lightgray') 
+            box.style.removeProperty('background');
+    }
+    
+    // Wait until animation is done playing
+    setTimeout(function () {
+        etchToy.classList.remove('shake');
+    }, 800);
     });
 }
 
-function randomizeColor() {
-    
+function addTrail(color, manyColors) {
+    // Event delegation
+    document.querySelector('.etch-a-sketch').addEventListener('mouseover', function(e) {
+        // e.target was the mouseovered div box element
+        if (manyColors) {
+            // Grab random Hex color code and change background to it
+            let hexColor = Math.floor(Math.random()*16777215).toString(16);
+            e.target.style.background = `#${hexColor}`;
+        } else {
+            e.target.style.background = `${color}`;
+        }
+    }, false);
+}
+
+// Grab a random color in either HSL or RGB or HEX on button press
+function clickColorButton() {
+    let singleColorOnly = false;
+    let manyColors = true;
+
+    document.querySelector('.hsl-button').addEventListener('click', function(e) {
+        let hslColor = `hsl(${random(0, 360)}, ${random(0, 100)}%, ${random(0, 100)}%)`;
+        addTrail(hslColor, singleColorOnly);
+    });
+
+    document.querySelector('.default-button').addEventListener('click', function(e) { 
+        addTrail("black", singleColorOnly);
+    });
+
+    // In HEX (which is the same colorwise as RGB, but let's try it)
+    document.querySelector('.rainbow-vomit').addEventListener('click', function(e) {
+        addTrail(``, manyColors);
+    });
+}
+
+// Grab a random number between a lower bound and upper bound
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Allow user to choose the size of the grid
+function inputGridSize() {
+    let inputs = document.querySelectorAll('input');
+
+    // Check if input is empty or is not a number
+    document.querySelectorAll('.arrow-submit')[0].addEventListener('click', function(e) {
+
+        // Loop through NodeList and remove all Etch-A-Sketch boxes
+        document.querySelectorAll('.box').forEach(e => e.remove());
+
+        createGrid(parseInt(inputs[0].value));
+    });
+}
+
+// Choose a color from HTMl input type="color"
+function inputColor() {
+    let inputs = document.querySelector('.pick-color');
+    let singleColorOnly = false;
+
+    document.querySelectorAll('.arrow-submit')[1].addEventListener('click', function(e) {
+        addTrail(inputs.value, false);
+    });
 }
 
 function main() {
-    createGrid(64);
-    resetGrid('blue');
-    addTrail('blue');
+    let initialColor = 'black';
+    let initialSize = 64;
+
+    // Size and colors on page load
+    createGrid(initialSize);
+    addTrail(initialColor);
+
+    // Grabs user input
+    inputGridSize();
+    inputColor();
+
+    // The buttons on the blue slant background
+    resetGrid(); 
+    clickColorButton();
 }
 
 main();
